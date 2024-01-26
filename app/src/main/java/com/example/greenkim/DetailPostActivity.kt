ab7bd9e7747f6e1a96d3865b326cc2f0c0aecc38
hoyeon <fnfn0901@sookmyung.ac.kr>
2024-01-26 10:05:39 +0900

    상세 게시글 동작 구현. 댓글, 게시글 받아와야함.

---
+++ app/src/main/java/com/example/greenkim/DetailPostActivity.kt
@@ -0,0 +1,11 @@
+package com.example.greenkim
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailPostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_post)
+    }
+}
