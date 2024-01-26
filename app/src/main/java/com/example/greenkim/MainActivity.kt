31e92530c1a0cbca13026d673a849469f22b53cf
yu-k-m <karen0806@naver.com>
2024-01-26 23:28:21 +0900

    메인 네비= 유경민

--- app/src/main/java/com/example/greenkim/MainActivity.kt
+++ app/src/main/java/com/example/greenkim/MainActivity.kt
@@ -19,18 +19,37 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Menu
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -42,6 +61,12 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat.startActivity
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.greenkim.data.BottomNavigationItem
 
 import com.example.greenkim.ui.theme.GreenKimTheme
 import com.example.greenkim.ui.theme.lineKorFamily
@@ -64,6 +89,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
     Column(modifier = Modifier.background(Color.White)) {
@@ -72,10 +98,94 @@ fun MainScreen(modifier: Modifier = Modifier) {
         CommunitySection()
         Spacer(modifier = modifier.size(40.dp))
         ZeroTodoSection()
+        //bottomApp()
     }
 
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun bottomApp() {
+    val items = listOf(
+        BottomNavigationItem(
+            title = "Home",
+            selectedIcon = Icons.Filled.Home,
+            unselectedIcon = Icons.Outlined.Home,
+            hasNews = false,
+        ),
+
+        BottomNavigationItem(
+            title = "Search",
+            selectedIcon = Icons.Filled.Menu,
+            unselectedIcon = Icons.Outlined.Menu,
+            hasNews = false,
+        ),
+        BottomNavigationItem(
+            title = "MyPage",
+            selectedIcon = Icons.Filled.Person,
+            unselectedIcon = Icons.Outlined.Person,
+            hasNews = true,
+        ),
+    )
+    var selectedItemIndex by rememberSaveable {
+        mutableStateOf(0)
+    }
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = {
+            NavigationBar {
+                val navBackStackEntry by navController.currentBackStackEntryAsState()
+                val currentDestination = navBackStackEntry?.destination
+                items.forEachIndexed { index, item ->
+                    NavigationBarItem(
+                        selected = selectedItemIndex == index,
+                        onClick = {
+                            selectedItemIndex = index
+                            navController.navigate(item.title) {
+                                popUpTo(navController.graph.findStartDestination().id) {
+                                    saveState = true
+                                }
+                                launchSingleTop = true
+                                restoreState = true
+                            }
+                        },
+                        icon = {
+                            BadgedBox(
+                                badge = {
+                                    if (item.badgeCount != null) {
+                                        Badge {
+                                            Text(item.badgeCount.toString())
+                                        }
+                                    } else if (item.hasNews) {
+                                        Badge()
+                                    }
+                                }) {
+                                Icon(
+                                    imageVector = if (index == selectedItemIndex) {
+                                        item.selectedIcon
+                                    } else item.unselectedIcon,
+                                    contentDescription = item.title
+                                )
+                            }
+                        })
+                }
+            }
+
+        }   ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = "Home",
+            Modifier.padding(innerPadding)
+        ) {
+            //composable 안되는 일반 뷰 파일이면 인텐트 갈길거
+            composable("Home") {  }
+            composable("Event") {  }
+            composable("Search") {  }
+            composable("MyPage") {  }
+        }
+
+    }
+}
 
 @Composable
 fun TopMainScreen(modifier: Modifier = Modifier) {
@@ -170,7 +280,7 @@ fun ZeroTodoSection() {
         Modifier
             .fillMaxWidth()
             .fillMaxHeight()
-            .padding(start = 30.dp)
+
     ) {
         Spacer(modifier = Modifier.size(15.dp))
         Row {
@@ -195,6 +305,7 @@ fun ZeroTodoSection() {
         }
         Spacer(modifier = Modifier.size(20.dp))
         ZeroActivities()
+        bottomApp()
     }
 }
 
