81ca2adb24e5172659d4524debd825ef0782b87e
janguzin <139611151+janguzin@users.noreply.github.com>
2024-02-02 16:25:50 +0900

    프로필 뱃지 없고 달력까지 완성

---
+++ app/build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.example.newgreenkim"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.newgreenkim"
+        minSdk = 33
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
+
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("com.kizitonwose.calendar:view:2.4.1")
+}
---
+++ app/src/main/java/com/example/newgreenkim/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.example.newgreenkim
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.widget.SwitchCompat
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting);
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+
+
+        val textView1: TextView = findViewById(R.id.textView1)
+        val switchView: SwitchCompat = findViewById(R.id.switchView1)
+
+        switchView.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                textView1.text = "댓글 알림"
+            } else {
+                textView1.text = "댓글 알림"
+            }
+        }
+
+
+
+
+        val testButton: Button = findViewById<Button>(R.id.button2)
+        testButton.setOnClickListener {
+            val nextIntent = Intent(this, MyPageActivity1::class.java)
+            startActivity(nextIntent)
+        }
+
+        val testButton1: Button = findViewById<Button>(R.id.button3)
+        testButton1.setOnClickListener {
+            val nextIntent = Intent(this, MyPageActivity2::class.java)
+            startActivity(nextIntent)
+        }
+
+
+
+
+
+
+
+        val testButton4: Button = findViewById<Button>(R.id.button7)
+        testButton4.setOnClickListener {
+            val nextIntent = Intent(this, WithdrawActivity::class.java)
+            startActivity(nextIntent)
+        }
+
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+}
